e098d1d0f1510a4e11b39ad3bf766adb9bc04e0b
Juvraj Singh <juvraj@gmail.com>
2023-03-09 21:49:54 +0000

    Project description added

---
+++ randomPasswordGenerator.py
@@ -0,0 +1,9 @@
+"""
+Generate a random password 8 characters long everytime the programme is run
+Password will include:
+    2 uppercase letters
+    2 lowercase letters
+    2 digits
+    2 punctuation signs
+These characters will appear in a random order
+"""
