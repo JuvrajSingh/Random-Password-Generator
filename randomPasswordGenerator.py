25437462b9cce14a25991e9e5d4bac450dd95584
Juvraj Singh <juvraj@gmail.com>
2023-03-10 14:57:39 +0000

    Changed from using ASCII to using string module, and finished generateCharacter function

--- randomPasswordGenerator.py
+++ randomPasswordGenerator.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 """
 Generate a random password 8 characters long everytime the programme is run
@@ -6,27 +7,21 @@ Password will include:
     2 uppercase letters
     2 lowercase letters
     2 digits
-    2 punctuation signs
+    2 symbols
 These characters will appear in a random order
 """
 
 characterTypes = {
-    "uppercaseLetter": [65, 90],
-    "lowercaseLetter": [97, 122],
-    "digit": [48, 57],
-    "punctuationSign": [[33, 47], [58, 64], [91, 96], [123, 126]]
+    "uppercaseLetter": string.ascii_uppercase,
+    "lowercaseLetter": string.ascii_lowercase,
+    "digit": string.digits,
+    "symbol": string.punctuation
 }
 
 def generateCharacter(characterType):
     """
     Generates a random character given the type
     """
-
-    asciiCodes = characterTypes[characterType]
-
-    if isinstance(asciiCodes[0], int):
-        character = chr(random.randint(asciiCodes[0], asciiCodes[1]))
-    else:
-        #todo
-        return
+    characterChoices = characterTypes[characterType]
+    character = random.choice(characterChoices)
     return character
