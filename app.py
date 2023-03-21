3c5e28493b846b5e879de2ec80c3cc76fc1e172e
Juvraj Singh <juvraj@gmail.com>
2023-03-21 12:22:34 +0000

    Started implementing code in index function to respond to form inputs, but still some bugs present

--- app.py
+++ app.py
@@ -6,8 +6,46 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     """Generates Random Password"""
+    """ADD COMMENTS TO CODE"""
+
+    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        password = generate_password()
+        length = request.form.get("length")
+        if not length:
+            # TO DO
+            return
+        else:
+            length = int(length)
+        
+        if request.form.get("digits_required"):
+            digits = request.form.get("digits")
+            if not digits:
+                # TO DO
+                return
+            else:
+                digits = int(digits)
+        else:
+            digits = 0
+
+        if request.form.get("symbols_required"):
+            symbols = request.form.get("symbols")
+            if not symbols:
+                # TO DO
+                return
+            else:
+                symbols = int(symbols)
+        else:
+            symbols = 0
+        
+        uppercase = 2
+        lowercase = 2
+
+        min_length = digits + symbols + uppercase + lowercase
+
+        if length < min_length:
+            length = min_length
+
+        password = generate_password(length, uppercase, lowercase, digits, symbols)
         return render_template("index.html", password=password)
     else:
         return render_template("index.html", password="")
